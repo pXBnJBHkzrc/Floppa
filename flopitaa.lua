6547e8ca05f51910f522ba25707391e63ab4c60c
UnknowBest <103254965+UnknowBest@users.noreply.github.com>
2024-03-26 08:03:06 -0500

    Update flopitaa.lua

--- flopitaa.lua
+++ flopitaa.lua
@@ -558,7 +558,7 @@ function OrionLib:MakeWindow(WindowConfig)
 				Size = UDim2.new(0, 32, 0, 32),
 				Position = UDim2.new(0, 10, 0.5, 0)
 			}), {
-				SetProps(MakeElement("Image", "rbxassetid://12614663538"), {
+				SetProps(MakeElement("Image", "rbxassetid://7733765398"), {
 					Size = UDim2.new(1, 0, 1, 0)
 				}),
 				AddThemeObject(SetProps(MakeElement("Image", "rbxassetid://4031889928"), {
